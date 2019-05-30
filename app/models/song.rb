b69e85f9e9c3fea860a6d8400e092e523ae6a33a
James Cox <jamesncox@yahoo.com>
2019-05-30 15:07:55 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,9 +2,9 @@ class Song < ActiveRecord::Base
   belongs_to :artist 
   belongs_to :genre 
   
-  attr_accessor :name 
+  # attr_accessor :name 
   
-  def initialize(name)
-    @name = name 
-  end 
+  # def initialize(name)
+  #   @name = name 
+  # end 
 end
